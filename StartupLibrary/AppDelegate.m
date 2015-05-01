f47c3c9143c7ebe1af9ab07f04afbe7ef82e313b
pabloparejo <parejo@iMac-de-parejo.local>
2015-05-01 15:11:26 +0200

    Setting up navVC as root

--- StartupLibrary/AppDelegate.m
+++ StartupLibrary/AppDelegate.m
@@ -7,7 +7,7 @@
 //
 
 #import "AppDelegate.h"
-
+#import "PARBookViewController.h"
 @interface AppDelegate ()
 
 @end
@@ -16,7 +16,14 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.backgroundColor = [UIColor whiteColor];
+    
+    PARBookViewController *bookVC = [[PARBookViewController alloc] init];
+    UINavigationController *navVC = [[UINavigationController alloc] initWithRootViewController:bookVC];
+    [self.window setRootViewController:navVC];
+    
+    [self.window makeKeyAndVisible];
     return YES;
 }
 
