f739fc426b267a024c0a5f97b0e8bc1f72cb627b
pabloparejo <parejo@iMac-de-parejo.local>
2015-05-01 15:08:19 +0200

    adding model

---
+++ StartupLibrary/PARBook.h
@@ -0,0 +1,13 @@
+//
+//  PARBook.h
+//  StartupLibrary
+//
+//  Created by parejo on 1/5/15.
+//  Copyright (c) 2015 PabloParejo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PARBook : NSObject
+
+@end
