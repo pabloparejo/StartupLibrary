950089679112a7c95133136958d04073b430fa7a
pabloparejo <parejo@iMac-de-parejo.local>
2015-05-01 15:08:57 +0200

    Book initializers

--- StartupLibrary/PARBook.h
+++ StartupLibrary/PARBook.h
@@ -6,8 +6,34 @@
 //  Copyright (c) 2015 PabloParejo. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+@import Foundation;
 
 @interface PARBook : NSObject
+@property (strong, nonatomic) NSString *title;
+@property (strong, nonatomic) NSString *author;
+@property (strong, nonatomic) NSURL *bookURL;
+@property (strong, nonatomic) NSURL *coverURL;
+@property (strong, nonatomic) NSString *summary;
+@property (strong, nonatomic) NSURL *webURL;
+@property (strong, nonatomic) NSString *category;
+@property (copy, nonatomic) NSArray *tags;
+
+-(id) initWithTitle:(NSString *)title
+             author:(NSString *)author
+            bookURL:(NSURL *)bookURL
+           coverURL:(NSURL *)coverURL
+            summary:(NSString *)summary
+             webURL:(NSURL *)webURL
+           category:(NSString *)category
+               tags:(NSArray *)tags;
+
++(instancetype) bookWithTitle:(NSString *)title
+                       author:(NSString *)author
+                      bookURL:(NSURL *)bookURL
+                     coverURL:(NSURL *)coverURL
+                      summary:(NSString *)summary
+                       webURL:(NSURL *)webURL
+                     category:(NSString *)category
+                         tags:(NSArray *)tags;
 
 @end
--- StartupLibrary/PARBook.m
+++ StartupLibrary/PARBook.m
@@ -10,4 +10,44 @@
 
 @implementation PARBook
 
+-(id) initWithTitle:(NSString *)title
+             author:(NSString *)author
+            bookURL:(NSURL *)bookURL
+           coverURL:(NSURL *)coverURL
+            summary:(NSString *)summary
+             webURL:(NSURL *)webURL
+           category:(NSString *)category
+               tags:(NSArray *)tags{
+    if (self = [super init]){
+		_title = title;
+		_author = author;
+		_bookURL = bookURL;
+		_coverURL = coverURL;
+		_summary = summary;
+		_webURL = webURL;
+		_category = category;
+		_tags = tags;	
+	}
+    return self;
+}
+
++(instancetype) bookWithTitle:(NSString *)title
+                       author:(NSString *)author
+                      bookURL:(NSURL *)bookURL
+                     coverURL:(NSURL *)coverURL
+                      summary:(NSString *)summary
+                       webURL:(NSURL *)webURL
+                     category:(NSString *)category 
+                         tags:(NSArray *)tags{
+
+    return [[self alloc] initWithTitle:title
+                                author:author
+                               bookURL:bookURL
+                              coverURL:coverURL
+                               summary:summary
+                                webURL:webURL
+                              category:category
+                                  tags:tags];
+}
+
 @end
