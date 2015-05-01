fb9e86ce3cd516e954587d8f838e9c50a11d5099
pabloparejo <parejo@iMac-de-parejo.local>
2015-05-01 14:28:16 +0200

    Initial Commit

---
+++ StartupLibrary/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  StartupLibrary
+//
+//  Created by parejo on 1/5/15.
+//  Copyright (c) 2015 PabloParejo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
