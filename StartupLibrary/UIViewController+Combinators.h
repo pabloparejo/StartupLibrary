21dee931cc09d36e07596b1aad92514421a7ba05
pabloparejo <parejo.p@gmail.com>
2015-05-02 23:41:49 +0200

    wrappedInNavVC category

---
+++ StartupLibrary/UIViewController+Combinators.h
@@ -0,0 +1,13 @@
+//
+//  UIViewController+Combinators.h
+//  StartupLibrary
+//
+//  Created by parejo on 2/5/15.
+//  Copyright (c) 2015 PabloParejo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIViewController_Combinators : UIViewController
+
+@end
---
+++ StartupLibrary/UIViewController+Combinators.m
@@ -0,0 +1,37 @@
+//
+//  UIViewController+Combinators.m
+//  StartupLibrary
+//
+//  Created by parejo on 2/5/15.
+//  Copyright (c) 2015 PabloParejo. All rights reserved.
+//
+
+#import "UIViewController+Combinators.h"
+
+@interface UIViewController_Combinators ()
+
+@end
+
+@implementation UIViewController_Combinators
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
