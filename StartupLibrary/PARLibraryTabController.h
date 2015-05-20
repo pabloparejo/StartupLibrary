7d9abe4e8056c6a2a4c9e2d758227386d51dd412
pabloparejo <parejo.p@gmail.com>
2015-05-20 19:37:03 +0200

    added custom tabBarController

---
+++ StartupLibrary/PARLibraryTabController.h
@@ -0,0 +1,13 @@
+//
+//  PARLibraryTabController.h
+//  StartupLibrary
+//
+//  Created by Pablo Parejo Camacho on 19/5/15.
+//  Copyright (c) 2015 PabloParejo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PARLibraryTabController : UITabBarController
+
+@end
---
+++ StartupLibrary/PARLibraryTabController.m
@@ -0,0 +1,13 @@
+//
+//  PARLibraryTabController.m
+//  StartupLibrary
+//
+//  Created by Pablo Parejo Camacho on 19/5/15.
+//  Copyright (c) 2015 PabloParejo. All rights reserved.
+//
+
+#import "PARLibraryTabController.h"
+
+@implementation PARLibraryTabController
+
+@end
